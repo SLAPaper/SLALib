e369687a98ebe4ad95da805f4b5eeb1fe4cfff52
SLAPaper <slapaper.pku@gmail.com>
2021-04-19 00:00:27 +0800

    add pages

---
+++ pages/list/list.vue
@@ -0,0 +1,47 @@
+<template>
+    <view>
+        <!-- <u-action-sheet :list="list" v-model="show"></u-action-sheet> -->
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [
+                {
+                    text: '点赞',
+                    color: 'blue',
+                    fontSize: 28
+                },
+                {
+                    text: '分享'
+                },
+                {
+                    text: '评论'
+                }
+            ],
+            show: true
+        };
+    },
+    onTabItemTap: function(e) {
+        console.log(e);
+    },
+    onShow: function() {
+        console.log('List Page Show');
+
+        // #ifdef MP-BAIDU
+        console.log('Baidu setPageInfo');
+        swan.setPageInfo({
+            title: '纸睡图书馆-藏书馆',
+            keywords: '图书管理',
+            description: '你的私人藏书管家',
+            image: 'static/logo.png'
+        });
+        // #endif
+    },
+    methods: {}
+};
+</script>
+
+<style></style>
